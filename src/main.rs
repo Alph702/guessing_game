5ad868e4d6db8c9165a963a8beb035f29f612407
Alph702 <alph702.12@gmail.com>
2025-09-05 14:17:48 +0500

    Generating random number

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::random_range(1..=100);
+    // println!("Secret number is: {secret_number}");
+
+
     let mut guess = String::new();
 
     println!("Enter your guess:");
