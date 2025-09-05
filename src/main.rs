ab1248ed677ba70d39f245b2ef89138a9ba645da
Alph702 <alph702.12@gmail.com>
2025-09-05 12:48:53 +0500

    geting user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let mut guess = String::new();
+
+    println!("Enter your guess:");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falled to get your guess");
+
+    println!("You guessed: {guess}");
+
 }
